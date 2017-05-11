486d7f67cf6d4c87fb37c0e7d3afa2237f526fb1
Curtainz <978041394@qq.com>
2017-05-11 13:37:10 +0800

    '调整search-input文件格式'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import {AppRoutingModule} from './app-routing.module';
-import {SearchInputComponent} from './commonComponent/search-input.component';
 import {Ng2PaginationModule} from 'ng2-pagination/index';
 import { UserCenterComponent } from './user-center/user-center.component';
 import {MyServiceService} from './core/app.service';
@@ -14,6 +13,7 @@ import {IndexComponent} from './search/index/index.component';
 import {DatalistComponent} from './search/datalist/datalist.component';
 import {DataDetailComponent} from './search/datalist/deta-detail/data-detail.component';
 import {Ng2UeditorModule} from 'ng2-ueditor/src/index';
+import {SearchInputComponent} from "./search/search-input/search-input.component";
 
 @NgModule({
   declarations: [
