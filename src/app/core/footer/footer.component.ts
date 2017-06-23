64ed12f3384105619dc39e6e7de07fb9704bebc2
pengdaojing <pengdj@cjtc.net.cm>
2017-06-23 18:41:41 +0800

    修改版本号

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -9,7 +9,7 @@ import {Router, NavigationStart} from "@angular/router";
                   <li><a routerLink="/service" [target]="'_blank'">服务协议</a></li>
                   <li><a routerLink="/privacy" [target]="'_blank'">隐私协议</a></li>
                   </ul>
-                  <p>版权所有 : 长江众创 Copyright@2010 All Rights Reserved 版本 1.0.1</p>
+                  <p>版权所有 : 长江众创 Copyright@2010 All Rights Reserved 版本 1.0.2</p>
              </div></div>`
 })
 
