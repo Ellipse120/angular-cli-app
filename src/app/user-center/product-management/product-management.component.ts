d691b6f14de79b919c13f153224ccf9dca2c4bd5
cjzc <lusai@cjzc.net.cn>
2017-07-07 17:34:15 +0800

    # modify product management tags and list's page CSS.

--- src/app/user-center/product-management/product-management.component.ts
+++ src/app/user-center/product-management/product-management.component.ts
@@ -1,14 +1,31 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {Router} from "@angular/router";
 
 @Component({
   templateUrl: './product-management.component.html',
   styleUrls: ['./product-management.component.css']
 })
 
-export class ProductManagementComponent {
+export class ProductManagementComponent implements OnInit {
 
-  userTag = [
-    {text: '产品列表', path: 'list'},
-    {text: '纠错处理', path: 'errata'}
-  ];
+  userTag = [];
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (this.router.url.split('/').includes('import')) {
+        this.userTag = [{text: '产品录入', path: 'import'}];
+      } else if (this.router.url.split('/').includes('edit')) {
+        this.userTag = [{text: '产品修改', path: 'edit'}];
+      } else {
+        this.userTag = [
+          {text: '产品列表', path: 'list'},
+          {text: '纠错处理', path: 'errata'}
+        ];
+      }
+    });
+  }
+
 }
