053ac2c9dbf47e527a6a8f096e0fa43d780096a8
Liangdi <wu@liangdi.me>
2017-07-13 01:27:41 +0800

    代码格式化

--- src/app/product-mangement/product-routing.module.ts
+++ src/app/product-mangement/product-routing.module.ts
@@ -1,16 +1,16 @@
 import {NgModule} from '@angular/core';
-import {RouterModule,Routes} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 
 
 import {ProductListComponent} from './product-list/product-list.component';
 import {ErrorCorrectComponent} from './error-correct/error-correct.component';
-import {ProductMangementComponent} from "./product-mangement.component";
+import {ProductMangementComponent} from './product-mangement.component';
 
 
 // 定义产品管理嵌套路由
-const childRoutes:Routes = [
+const childRoutes: Routes = [
   // 重定向路由
   {
     path: 'productlist',
@@ -21,15 +21,15 @@ const childRoutes:Routes = [
     component: ErrorCorrectComponent
   },
   {
-    path: '**',redirectTo: 'productlist'
+    path: '**', redirectTo: 'productlist'
   }
 ];
 
-const productRouters:Routes = [
+const productRouters: Routes = [
   // 重定向路由
 
-  {path:'promange',component:ProductMangementComponent, children: childRoutes}
- ]
+  {path: 'promange', component: ProductMangementComponent, children: childRoutes}
+ ];
 
 // @NgModule是一个装饰函数，它接收一个用来描述模块属性的元数据对象
 @NgModule({
