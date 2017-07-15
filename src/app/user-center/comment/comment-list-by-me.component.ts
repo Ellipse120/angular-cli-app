c1dacff04fbb0d16688ef4f2e54614457c22035f
pengdaojing <pengdj@cjzc.net.cn>
2017-07-15 14:08:48 +0800

    评论样式

--- src/app/user-center/comment/comment-list-by-me.component.ts
+++ src/app/user-center/comment/comment-list-by-me.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-comment-list-by-me',
   templateUrl: './comment-list-by-me.component.html',
-  styleUrls: ['./comment-list-by-me.component.css']
+  styleUrls: ['./comment.component.css']
 })
 export class CommentListByMeComponent implements OnInit {
-  comments = [];
+  comments = [
+    {averageScore: 4.5, scoreOnTimeliness: 4.5, scoreOnAccuracy: 4.5, scoreOnNormalization: 4, scoreOnIntegrity: 5},
+    {averageScore: 4.5, scoreOnTimeliness: 4.5, scoreOnAccuracy: 4.5, scoreOnNormalization: 4, scoreOnIntegrity: 5}
+    ];
+  averageScore: Array<any>;
   constructor() { }
 
   ngOnInit() {
+    this.averageScore = Array(5).fill(1).map((x, i) => i);
   }
 
 }
--- src/app/user-center/comment/comment-list.component.ts
+++ src/app/user-center/comment/comment-list.component.ts
@@ -3,14 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-comment-list',
   templateUrl: './comment-list.component.html',
-  styleUrls: ['./comment-list.component.css']
+  styleUrls: ['./comment.component.css']
 })
 export class CommentListComponent implements OnInit {
 
-  comments = [];
+  comments = [
+    {averageScore: 4.5, scoreOnTimeliness: 4.5, scoreOnAccuracy: 4.5, scoreOnNormalization: 4, scoreOnIntegrity: 5},
+    {averageScore: 4.5, scoreOnTimeliness: 4.5, scoreOnAccuracy: 4.5, scoreOnNormalization: 4, scoreOnIntegrity: 5}
+    ];
+  averageScore: Array<any>;
   constructor() { }
 
   ngOnInit() {
+    this.averageScore = Array(5).fill(1).map((x, i) => i);
   }
 
 }
