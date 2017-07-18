c0fdd7c4df015257de5c93bb26a767bff7c109f8
pengdaojing <pengdj@cjzc.net.cn>
2017-07-18 15:36:30 +0800

    修改产品管理产品列表样式

--- src/app/operation-management/product-management/product-list.component.ts
+++ src/app/operation-management/product-management/product-list.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 
 @Component({
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-management.component.css']
+  styleUrls: ['../operation-management.component.css']
 })
 
 export class OperationProductListComponent implements OnInit {
