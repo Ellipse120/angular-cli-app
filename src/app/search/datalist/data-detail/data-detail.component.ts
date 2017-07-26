3bf6efbfef3ec6c9e61858c2b40030e2fc238ec0
Liangdi <wu@liangdi.me>
2017-07-26 12:53:25 +0800

    sampleFilePath undefined 判断处理

--- src/app/search/datalist/data-detail/data-detail.component.ts
+++ src/app/search/datalist/data-detail/data-detail.component.ts
@@ -102,7 +102,11 @@ export class DataDetailComponent implements OnInit {
             this.productDetail = res;
             this.productDetail.premium = this.productDetail.premium ? '是' : '否';
             this.productDetail.modifiedOn = this.commonService.getDateForDay(this.productDetail.modifiedOn);
-            this.downloadUrl = 'ysl-ws/api/file/' + this.productDetail.sampleFilePath.split('.')[0] + '/download';
+
+            if ( this.productDetail.sampleFilePath ) {
+               this.downloadUrl = 'ysl-ws/api/file/' + this.productDetail.sampleFilePath.split('.')[0] + '/download';
+
+            }
             for (const key in advancedKey) {
               if (advancedKey.hasOwnProperty(key)) {
                 switch (key) {
